4f81a0878751b95b75073bad2dc4cae1a4b06a42
Adrian Perez <perezadrian.m@hotmail.com>
2024-01-17 02:59:16 -0300

    Added toggle minimize-maximize sidebar

--- frontend/py/main.py
+++ frontend/py/main.py
@@ -4,3 +4,8 @@ from pyscript import document , when
 def switch_mode(event):
     #Comentario
     document.querySelector('body').classList.toggle('dark')
+
+@when("click", "toggle")
+def switch_toggle(event):
+    #Comentario
+    document.querySelector('aside').classList.toggle('close')
