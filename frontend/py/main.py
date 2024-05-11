7e58bd04a1842d0f1b11bbc5603ae967e8bbce15
Adrian Perez <perezadrian.m@hotmail.com>
2024-05-11 03:52:53 -0300

    COmmit para reacomodar todo. Ni idea que hay bien o mal. Limpio a partir de aca

--- frontend/py/main.py
+++ frontend/py/main.py
@@ -1,12 +1,12 @@
 from pyscript import document , when
 
 @when("click", "switch")
-def switch_mode(event):
+def switch_mode(event)git:
     #Comentario
     document.querySelector('body').classList.toggle('dark')
 
 @when("click", "toggle")
 def switch_toggle(event):
     #Comentario
-    document.querySelector('aside').classList.toggle('close')
+    document.querySelector('div').classList.toggle('close')
 
